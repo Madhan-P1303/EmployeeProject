dc8970e2a1b87c7c02208bbff7323a53e9074876
Madhan-P1303 <madhan93454@gmail.com>
2025-07-07 11:16:49 +0530

    feat:fetchall endpoint is added

--- SpringBootCrudExample/src/main/java/com/example/employee/service/EmployeeServiceInt.java
+++ SpringBootCrudExample/src/main/java/com/example/employee/service/EmployeeServiceInt.java
@@ -1,13 +1,15 @@
 package com.example.employee.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
-
 import com.example.employee.entity.Employee;
 import com.example.employee.response.EmployeeResponse;
 
 @Service
 public interface EmployeeServiceInt {
-	
-	public EmployeeResponse createEmployee(Employee employee);
-
+    
+    public EmployeeResponse createEmployee(Employee employee);
+    
+    public EmployeeResponse getAllEmployees();
+ 
 }
